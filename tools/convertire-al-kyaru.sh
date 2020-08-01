5451cb3dbd32095fbd94787d7be74f3773beeeeb
Gustavo Reis <gusbemacbe@gmail.com>
2020-08-01 00:10:40 -0300

    Piccolo upgrade di kYaru++

---
+++ tools/convertire-al-kyaru.sh
@@ -0,0 +1,8 @@
+sed --in-place --follow-symlinks 's/gray/#444444/g' *.svg
+sed --in-place --follow-symlinks 's/#c4c4c4/#444444/g' *.svg
+sed --in-place --follow-symlinks 's/#7d7d7d/#444444/g' *.svg
+sed --in-place --follow-symlinks 's/#757575/#444444/g' *.svg
+sed --in-place --follow-symlinks 's/#828282/#444444/g' *.svg
+sed --in-place --follow-symlinks 's/#858585/#444444/g' *.svg
+sed --in-place --follow-symlinks 's/#808080/#444444/g' *.svg
+sed --in-place --follow-symlinks 's/#888/#444444/g' *.svg
