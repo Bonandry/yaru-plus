294c2cee8d071b2c5ef2686e854f31c3e1aaa4f3
Gustavo Reis <gusbemacbe@gmail.com>
2020-08-01 03:04:43 -0300

    Piccolo upgrade di Yaru++ Scuro

---
+++ tools/convertire-allo-scuro.sh
@@ -0,0 +1,8 @@
+sed --in-place --follow-symlinks 's/gray/#f2f2f7/g' *.svg
+sed --in-place --follow-symlinks 's/#c4c4c4/#f2f2f7/g' *.svg
+sed --in-place --follow-symlinks 's/#7d7d7d/#f2f2f7/g' *.svg
+sed --in-place --follow-symlinks 's/#757575/#f2f2f7/g' *.svg
+sed --in-place --follow-symlinks 's/#828282/#f2f2f7/g' *.svg
+sed --in-place --follow-symlinks 's/#858585/#f2f2f7/g' *.svg
+sed --in-place --follow-symlinks 's/#808080/#f2f2f7/g' *.svg
+sed --in-place --follow-symlinks 's/#888/#f2f2f7/g' *.svg
