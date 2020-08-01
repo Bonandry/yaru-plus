1d4e7c8213cb0aa0289deb77309f6c97f6d997ab
Gustavo Reis <gusbemacbe@gmail.com>
2020-08-01 03:45:29 -0300

    Corretti i colori sbagliati delle icone monocromatiche di Yaru++

---
+++ tools/correggere-i-colori-sbagliati.sh
@@ -0,0 +1,7 @@
+sed --in-place --follow-symlinks 's/gray/#808080/g' *.svg
+sed --in-place --follow-symlinks 's/#c4c4c4/#808080/g' *.svg
+sed --in-place --follow-symlinks 's/#7d7d7d/#808080/g' *.svg
+sed --in-place --follow-symlinks 's/#757575/#808080/g' *.svg
+sed --in-place --follow-symlinks 's/#828282/#808080/g' *.svg
+sed --in-place --follow-symlinks 's/#858585/#808080/g' *.svg
+sed --in-place --follow-symlinks 's/#888/#808080/g' *.svg
