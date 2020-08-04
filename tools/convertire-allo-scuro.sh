f9a6e46b91bfca2a7e2a320d26b3cf1376b9cc45
Gustavo Reis <gusbemacbe@gmail.com>
2020-08-04 06:35:36 -0300

    Aggiunte le icone di Markets

--- tools/convertire-allo-scuro.sh
+++ tools/convertire-allo-scuro.sh
@@ -1,5 +1,6 @@
 sed --in-place --follow-symlinks 's/gray/#f2f2f7/g' *.svg
 sed --in-place --follow-symlinks 's/#c4c4c4/#f2f2f7/g' *.svg
+sed --in-place --follow-symlinks 's/#444444/#f2f2f7/g' *.svg
 sed --in-place --follow-symlinks 's/#7d7d7d/#f2f2f7/g' *.svg
 sed --in-place --follow-symlinks 's/#757575/#f2f2f7/g' *.svg
 sed --in-place --follow-symlinks 's/#828282/#f2f2f7/g' *.svg
--- tools/correggere-i-colori-sbagliati.sh
+++ tools/correggere-i-colori-sbagliati.sh
@@ -1,5 +1,7 @@
 sed --in-place --follow-symlinks 's/gray/#808080/g' *.svg
 sed --in-place --follow-symlinks 's/#c4c4c4/#808080/g' *.svg
+sed --in-place --follow-symlinks 's/#f2f2f7/#808080/g' *.svg
+sed --in-place --follow-symlinks 's/#474747/#808080/g' *.svg
 sed --in-place --follow-symlinks 's/#7d7d7d/#808080/g' *.svg
 sed --in-place --follow-symlinks 's/#757575/#808080/g' *.svg
 sed --in-place --follow-symlinks 's/#828282/#808080/g' *.svg
