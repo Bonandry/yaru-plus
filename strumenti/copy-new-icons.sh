52b22adc74490d9d31204763e2203659bb1d7543
Gustavo Reis <gusbemacbe@gmail.com>
2020-12-06 17:32:48 -0300

    Icone di Pinta e Seahorse aggiornate dal @Bonandry

--- strumenti/copy-new-icons.sh
+++ strumenti/copy-new-icons.sh
@@ -1,9 +1,19 @@
 #!/bin/zsh
 
 COMPARAÇÕES="$HOME/Comparações/Yaru++ Development/Icons/" 
-ÍCONES="$HOME/Imagens/ícones/"
+ÍCONES="$HOME/Imagens/Ícones/"
 YARUO="$HOME/GitHub/yaru-plus/"
 YARUB="$HOME/GitHub/yaru-plus-beta/"
 YARUG="$HOME/GitHub/yaru-plus-gusbemacbe/"
 
+# External HD
+
+HDYARUO="/run/media/gusbemacbe/Gits/GitHub/Andrea/yaru-plus/"
+HDYARUB="/run/media/gusbemacbe/Gits/GitHub/Andrea/yaru-plus-beta/"
+HDYARUG="/run/media/gusbemacbe/Gits/GitHub/Andrea/yaru-plus-gusbemacbe/"
+
 # cd $YARUB
+
+cp $HDYARUB/Yaru++/apps/scalable/com.github.PintaProject.Pinta.svg $ÍCONES/apps/scalable
+cp $HDYARUB/Yaru++/apps/scalable/org.gnome.design.AppIconPreview.svg $ÍCONES/apps/scalable
+cp $HDYARUB/Yaru++/apps/scalable/org.gnome.seahorse.Application.svg $ÍCONES/apps/scalable
