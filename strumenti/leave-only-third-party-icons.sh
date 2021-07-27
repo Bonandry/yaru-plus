fd35fd690c2c2ddea9fcd7d95c6f6484aa19730b
Gustavo Costa <gusbemacbe@gmail.com>
2021-07-27 20:38:53 -0300

    Added a shell script file

--- strumenti/leave-only-third-party-icons.sh
+++ strumenti/leave-only-third-party-icons.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-yaru_original="/usr/share/icons/Yaru"
-yaru_plus="/home/gusbemacbe/GitHub/yaru-plus-gusbemacbe/Yaru++-Minimal"
+adwaita_original="/usr/share/icons/Adwaita"
+adwaita_plus="$HOME/GitHub/adwaita-plus-beta/Adwaita++-Minimal"
 
 # map the Yaru sizes to Yaru++ sizes
 declare -A size_mapping=([16x16]=16 [24x24]=24 [32x32]=32 [48x48]=48 [scalable]=symbolic)
@@ -11,10 +11,10 @@ for size in "${!size_mapping[@]}"
 do
     for dir in "${directories[@]}"
     do
-        for i in "$yaru_original/$size/$dir/"*.{png,svg}
+        for i in "$adwaita_original/$size/$dir/"*.{png,svg}
         do
             # Take just the filename ${i##*/} and build the new path
-            plus_file=$yaru_plus/$dir/${size_mapping["$size"]}/${i##*/}
+            plus_file=$adwaita_plus/$dir/${size_mapping["$size"]}/${i##*/}
             if [[ $plus_file = *.png ]]
             then
                 # replce the extension with svg
